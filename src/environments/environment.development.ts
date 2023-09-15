ec7c39f9468c4ad71b26460834f0cc91683fa7d6
Abdullah Mahmoud <abdullah.mahmoud.f196@gmail.com>
2023-09-15 23:52:32 +0300

    connect to api

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  BaseAPIUrl:'http://localhost:3300/'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  BaseAPIUrl:'http://localhost:3300/'
+};
