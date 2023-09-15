46ca50af7165dabd598a833101e61911b52424d4
OlaMohamedd <138399376+OlaMohamedd@users.noreply.github.com>
2023-09-15 23:53:21 +0300

    creating component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './Component/sidebar/sidebar.component';
+import { LoginComponent } from './Component/login/login.component';
+import { LogoutComponent } from './Component/logout/logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
