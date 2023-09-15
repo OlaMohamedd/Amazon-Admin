ffbb0e86c0a977c7ed44fa06c7816da543487970
FatemaAlsayed99 <141174900+FatemaAlsayed99@users.noreply.github.com>
2023-09-16 00:07:36 +0300

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './Component/sidebar/sidebar.component';
 import { LoginComponent } from './Component/login/login.component';
-import { LogoutComponent } from './Component/logout/logout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     LoginComponent,
-    LogoutComponent
   ],
   imports: [
     BrowserModule,
