95c7b1e47c75e0d3ad6eaa870cb7b5e8d1e32df1
ahmed elhoseiny <ahmedelhoseiny5555@gmail.com>
2023-09-16 13:12:27 +0300

    components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CoupensComponent } from './coupens/coupens.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { MediaComponent } from './media/media.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProductsComponent } from './products/products.component';
-import { SettingsComponent } from './settings/settings.component';
-import { StatisticsComponent } from './statistics/statistics.component';
+import { CoupensComponent } from './components/coupens/coupens.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MediaComponent } from './components/media/media.component';
+import { PagesComponent } from './components/pages/pages.component';
+import { ProductsComponent } from './components/products/products.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: 'dashboard', component: DashboardComponent},
   {
     path: 'products',
-    loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
+    loadChildren: () => import('./components/products/products.module').then(m => m.ProductsModule)
   },
   {path: 'statistics', component: StatisticsComponent},
   {
     path: 'coupens',
-    loadChildren: () => import('./coupens/coupens.module').then(m => m.CoupensModule)
+    loadChildren: () => import('./components/coupens/coupens.module').then(m => m.CoupensModule)
   },
   {path: 'pages', component: PagesComponent},
   {path: 'media', component: MediaComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BodyComponent } from './body/body.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { StatisticsComponent } from './statistics/statistics.component';
-import { PagesComponent } from './pages/pages.component';
-import { MediaComponent } from './media/media.component';
-import { SettingsComponent } from './settings/settings.component';
-import { SublevelMenuComponent } from './sidenav/sublevel-menu.component';
+import { BodyComponent } from './components/body/body.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
+import { PagesComponent } from './components/pages/pages.component';
+import { MediaComponent } from './components/media/media.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { SublevelMenuComponent } from './components/sidenav/sublevel-menu.component';
+
 
 @NgModule({
   declarations: [
