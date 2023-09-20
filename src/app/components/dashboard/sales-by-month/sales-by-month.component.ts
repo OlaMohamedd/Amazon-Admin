38361f51fbd534164aff9a2ceb0fee3cab142ca0
OlaMohamedd <138399376+OlaMohamedd@users.noreply.github.com>
2023-09-20 03:15:34 +0300

    finish charts

--- src/app/components/dashboard/sales-by-month/sales-by-month.component.ts
+++ src/app/components/dashboard/sales-by-month/sales-by-month.component.ts
@@ -35,10 +35,10 @@ export class SalesByMonthComponent {
   // public barChartPlugins = [DataLabelsPlugin];
 
   public barChartData: ChartData<'bar'> = {
-    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    labels: ['2016', '2017', '2018', '2019', '2020', '2021', '2022'],
     datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Electronics' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Home Decor' },
     ],
   };
 
