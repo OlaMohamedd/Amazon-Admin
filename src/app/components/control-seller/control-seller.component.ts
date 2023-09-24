6b73cd752e34bbc71863c4311cd060ac88730bf9
FatemaAlsayed99 <141174900+FatemaAlsayed99@users.noreply.github.com>
2023-09-24 19:48:08 +0300

    edit seller fun

--- src/app/components/control-seller/control-seller.component.ts
+++ src/app/components/control-seller/control-seller.component.ts
@@ -8,7 +8,7 @@ import Swal from 'sweetalert2';
   styleUrls: ['./control-seller.component.scss']
 })
 export class ControlSellerComponent implements OnInit {
-  sellers: any=[];
+  sellers: any[];
 
   constructor(private sellerService: ControlSellerService) { }
 
@@ -18,36 +18,38 @@ export class ControlSellerComponent implements OnInit {
 
   loadSellers() {
     this.sellerService.getAllSellers().subscribe(
-      // next:(response) => {
-      //   this.sellers = [...Object.values(response.data)][0];
-      // },
-      // error:(error) => {
-      //   console.log(error);
-      // }
-      data =>{
-  
-        this.sellers=[...Object.values(data)][0];
-              // console.log(this.orders);
-
+      response => {
+        this.sellers = response.data;
+      },
+      error => {
+        console.log(error);
       }
-  );
+      // // data =>{
+  
+      //   this.sellers=[...Object.values(data)][0];
+      //         // console.log(this.orders);
+
+      // }
+    );
   }
 
-//   deleteSeller(sellerId: string) {
-
-//     this.sellerService.deleteSeller(sellerId).subscribe({
-//       next:(response) => {
-//         console.log(response);
-//         const index = this.sellers.findIndex(seller => seller._id === sellerId);
-//         if (index !== -1) {
-//           this.sellers[index].status = 'deleted';
-//           Swal.fire({title:'Update statue!',text:"Seller deleted", icon:'success'});
-//         }
-//       },
-//       error:(error) => {
-//         console.log(error);
-//       }
-//     });
+// deleteSeller(sellerId: string) {
+//   Swal.fire({
+//     title: 'Delete order!',
+//     text: 'Do you want to delete order',
+//     icon: 'question',
+//     confirmButtonText: 'Delete Order',
+//     cancelButtonText: 'Cancel',
+//     showCancelButton: true
+//   }).then((res) =>{
+//     if(res.isConfirmed){
+//       this.sellerService.deleteSeller(sellerId).subscribe(res=>{
+//         console.log(res);
+//         Swal.fire({title:'Order deleted!',text:"Order deleted", icon:'success'});
+//         this.loadSellers();
+//       })
+//     }
+//   })
 // }
 deleteSeller(sellerId: string) {
   Swal.fire({
@@ -57,28 +59,52 @@ deleteSeller(sellerId: string) {
     confirmButtonText: 'Delete Order',
     cancelButtonText: 'Cancel',
     showCancelButton: true
-  }).then((res) =>{
-    if(res.isConfirmed){
-      this.sellerService.deleteSeller(sellerId).subscribe(res=>{
-        console.log(res);
-        Swal.fire({title:'Order deleted!',text:"Order deleted", icon:'success'});
-        this.loadSellers();
-      })
+  }).then((res) => {
+    if (res.isConfirmed) {
+      this.sellerService.deleteSeller(sellerId).subscribe({
+       next:(response) => {
+          console.log(response);
+          const index = this.sellers.findIndex((seller) => seller._id === sellerId);
+          if (index !== -1) {
+            this.sellers[index].status = 'deleted';
+            Swal.fire({ title: 'Order deleted!', text: 'Order deleted', icon: 'success' });
+            this.loadSellers();
+          }
+        },
+       error: (error) => {
+          console.log(error);
+        }
+       } );
     }
-  })
- 
-
+  });
 }
-changeStatus(sellerId: string, status: string) {
-  if (status == 'warning') {
-    this.sellers.status = 'blocked';
-    this.sellerService.changeStatus(sellerId, status).subscribe(
-      res=>{
-        console.log(res);
-        this.loadSellers();
-      });
-  }
+// changeStatus(sellerId: string, status: string) {
+//   if (status == 'warning') {
+//     this.sellers.status = 'blocked';
+//     this.sellerService.changeStatus(sellerId, status).subscribe(
+//       res=>{
+//         console.log(res);
+//         this.loadSellers();
+//       });
+//   }
    
+//   }
+  changeStatus(sellerId: string, status: string) {
+    if (status == 'warning') {
+      this.sellerService.changeStatus(sellerId, status).subscribe(
+        (res) => {
+          console.log(res);
+          const index = this.sellers.findIndex((seller) => seller._id === sellerId);
+          if (index !== -1) {
+            this.sellers[index].status = 'blocked';
+            this.loadSellers();
+          }
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
   }
 }
 
