f9ae95b17919b8a2a419deda7658f67e6aa7a417
FatemaAlsayed99 <141174900+FatemaAlsayed99@users.noreply.github.com>
2023-09-26 15:57:38 +0300

    edit chart

--- src/app/components/dashboard/sales-by-month/sales-by-month.component.ts
+++ src/app/components/dashboard/sales-by-month/sales-by-month.component.ts
@@ -1,80 +1,70 @@
-import { Component, ViewChild } from '@angular/core';
-import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
-import { BaseChartDirective } from 'ng2-charts';
-// import { DataLabelsPlugin } from 'chartjs-plugin-datalabels';
-
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+import { OrderSevicesService } from 'src/app/Services/orderSevices/order-sevices.service';
+import { Order } from 'src/app/models/order';
 
 @Component({
   selector: 'app-sales-by-month',
   templateUrl: './sales-by-month.component.html',
   styleUrls: ['./sales-by-month.component.scss']
 })
-export class SalesByMonthComponent {
-  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+export class SalesByMonthComponent implements OnInit {
 
-  public barChartOptions: ChartConfiguration['options'] = {
-    responsive: true,
-    // We use these empty structures as placeholders for dynamic theming.
-    scales: {
-      x: {},
-      y: {
-        min: 10,
+  orders: Order[] = [];
+  chart: any; // Reference to the chart object
+  orderCounts: number[] = [];
+
+  constructor(private orderService: OrderSevicesService) {}
+
+  ngOnInit(): void {
+    this.orderService.getAllProducts().subscribe({
+      next: (data) => {
+        this.orders = data;
+        console.log(this.orders);
+        this.orderCounts = this.orders.map(
+          (p) =>
+            this.orders.filter((o) => o._id === p._id).length
+        );
+        // Create the chart
+        this.chart = new Chart('OrderChart', {
+          type: 'bar',
+          options: {
+            animation: false,
+            plugins: {
+              legend: {
+                display: false,
+              },
+              tooltip: {
+                enabled: false,
+              },
+            },
+          },
+          data: {
+            labels: this.orders.map((order) => order._id),
+            datasets: [
+              {
+                data: this.orderCounts,
+                backgroundColor: [
+                  'rgba(255, 99, 132, 0.5)',
+                  'rgba(54, 162, 235, 0.5)',
+                  'rgba(255, 206, 86, 0.5)',
+                ],
+                borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                ],
+                borderWidth: 1,
+              },
+            ],
+          },
+        });
       },
-    },
-    plugins: {
-      legend: {
-        display: true,
+      error: (err) => {
+        console.log('Error:', err);
       },
-      // DataLabelsPlugin: {
-      //   anchor: 'end',
-      //   align: 'end',
-      // },
-    },
-  };
-  public barChartType: ChartType = 'bar';
-  // public barChartPlugins = [DataLabelsPlugin];
-
-  public barChartData: ChartData<'bar'> = {
-    labels: ['2016', '2017', '2018', '2019', '2020', '2021', '2022'],
-    datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Electronics' },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Home Decor' },
-    ],
-  };
-
-  // events
-  public chartClicked({
-    event,
-    active,
-  }: {
-    event?: ChartEvent;
-    active?: object[];
-  }): void {
-    console.log(event, active);
+    });
   }
 
-  public chartHovered({
-    event,
-    active,
-  }: {
-    event?: ChartEvent;
-    active?: object[];
-  }): void {
-    console.log(event, active);
-  }
-
-  public randomize(): void {
-    // Only Change 3 values
-    this.barChartData.datasets[0].data = [
-      Math.round(Math.random() * 100),
-      59,
-      80,
-      Math.round(Math.random() * 100),
-      56,
-      Math.round(Math.random() * 100),
-      40,
-    ];
-
-    this.chart?.update();
-  }
+  
 }
